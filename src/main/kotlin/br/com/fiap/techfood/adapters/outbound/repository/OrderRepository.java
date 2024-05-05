f166495f032f3b08c7a042f49879cd088a403429
isabellelourenco <isabellelourenco02@gmail.com>
2024-05-05 19:59:07 -0300

    PRODUTO-01: Remove arquivo java

--- src/main/kotlin/br/com/fiap/techfood/adapters/outbound/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.techfood.adapters.outbound.repository;
-
-import br.com.fiap.techfood.adapters.outbound.repository.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
-}
