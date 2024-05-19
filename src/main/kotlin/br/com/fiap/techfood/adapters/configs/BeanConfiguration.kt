ee44eb098338a947593afe37b10badceffb83119
Marcelo C <marcelo.c.b22@gmail.com>
2024-05-19 15:00:51 -0300

    Separando config do Swagger em outra classe (arquivo).

--- src/main/kotlin/br/com/fiap/techfood/adapters/configs/BeanConfiguration.kt
+++ src/main/kotlin/br/com/fiap/techfood/adapters/configs/BeanConfiguration.kt
@@ -40,23 +40,3 @@ class BeanConfiguration {
     }
 
 }
-
-@Configuration
-class SwaggerConfiguration {
-    @Bean
-    fun springShopOpenAPI(): OpenAPI {
-        return OpenAPI()
-            .info(
-                Info()
-                    .title("Tech Food")
-                    .description("Tech Food backend application")
-            )
-        // Vou deixar para referencia futura:
-        //.externalDocs(
-        //    ExternalDocumentation()
-        //        .description("SpringShop Wiki Documentation")
-        //        .url("https://springshop.wiki.github.org/docs")
-        //)
-    }
-}
-
