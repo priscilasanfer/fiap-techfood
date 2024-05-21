0e3ae10d720e6d5503f85a1ca2f4edae3732b92a
Priscila Ferreira <priscilasanfer@gmail.com>
2024-05-21 17:31:00 -0300

    Atualiza validação de dados do cliente

--- src/main/kotlin/br/com/fiap/techfood/core/ports/outbound/repositories/ClientRepositoryCore.kt
+++ src/main/kotlin/br/com/fiap/techfood/core/ports/outbound/repositories/ClientRepositoryCore.kt
@@ -1,13 +1,13 @@
 package br.com.fiap.techfood.core.ports.outbound.repositories
 
 import br.com.fiap.techfood.core.application.domains.ClientDomain
-import org.springframework.data.domain.Pageable
+import br.com.fiap.techfood.core.application.domains.PageInfo
 import java.util.*
 
 interface ClientRepositoryCore {
     fun save(clientDomain: ClientDomain): ClientDomain
     fun findById(id: UUID): Optional<ClientDomain>
     fun findByCpf(cpf: String): Optional<ClientDomain>
-    fun findAll(pageable: Pageable): List<ClientDomain>
+    fun findAll(pageInfo: PageInfo): List<ClientDomain>
     fun delete(clientDomain: ClientDomain)
 }
